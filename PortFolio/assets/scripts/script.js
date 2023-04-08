60403fbcee647b03516db98ea4074db3e5b50ead
Karthikgohul <70338249+karthikgohul@users.noreply.github.com>
2023-04-08 20:43:42 +0530

    Add files via upload

---
+++ PortFolio/assets/scripts/script.js
@@ -0,0 +1,9 @@
+function navMob(){
+    var hamMenu=document.getElementById('nav-links')
+        if (hamMenu.style.display==="block"){
+            hamMenu.style.display="none";
+        }
+        else{
+            hamMenu.style.display="block";
+        }
+    }
