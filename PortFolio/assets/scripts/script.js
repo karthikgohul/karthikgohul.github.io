7d161da52b6b3b7b4d7954252ba63b69289fea34
Karthikgohul <70338249+karthikgohul@users.noreply.github.com>
2023-06-13 10:41:29 +0530

    Update script.js

--- PortFolio/assets/scripts/script.js
+++ PortFolio/assets/scripts/script.js
@@ -8,9 +8,18 @@ function closeNav() {
     close.classList.toggle('mobile-menu')
 }
 
+
 function collapseNavbar() {
     var navbar = document.getElementById("mobile-menu");
     if (window.innerWidth <= 768) {
       navbar.classList.toggle('mobile-menu')
     }
   }
+
+
+window.addEventListener("load",function(){
+  const preLoader=document.querySelector('.loading-container')
+  const body=document.querySelector('.theme')
+  preLoader.style.display="none"
+  body.style.display="block"
+});
